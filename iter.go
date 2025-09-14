3b771c33297fa3a6ddfd491e9265d0c4cb1ff3d0
Hiroaki Nakamura <hnakamur@gmail.com>
2025-09-15 00:07:56 +0900

    Add NewReadDirIterRecursiveSorted

--- iter.go
+++ iter.go
@@ -3,10 +3,12 @@
 package readdiriter
 
 import (
+	"cmp"
 	"errors"
 	"io"
 	"io/fs"
 	"iter"
+	"slices"
 )
 
 // ReadDirer is the interface that wraps the ReadDir method in io/fs.ReadDirFile
@@ -43,4 +45,30 @@ func NewReadDirIter(file ReadDirer, n int) iter.Seq2[fs.DirEntry, error] {
 			}
 		}
 	}
+}
+
+// NewReadDirIterSorted returns an iterate over directory entries from the file
+// parameter.
+//
+// Note: The directory entries are in lexical order.
+func NewReadDirIterSorted(file ReadDirer) iter.Seq2[fs.DirEntry, error] {
+	return func(yield func(fs.DirEntry, error) bool) {
+		de, err := file.ReadDir(0)
+		if err != nil {
+			yield(nil, err)
+			return
+		}
+		sortDirEntriesByName(de)
+		for _, e := range de {
+			if !yield(e, nil) {
+				return
+			}
+		}
+	}
+}
+
+func sortDirEntriesByName(de []fs.DirEntry) {
+	slices.SortFunc(de, func(a, b fs.DirEntry) int {
+		return cmp.Compare(a.Name(), b.Name())
+	})
 }
--- iter_recursive.go
+++ iter_recursive.go
@@ -33,7 +33,7 @@ func (e *DirAndEntry) Entry() fs.DirEntry {
 }
 
 // NewReadDirIterRecursive returns an iterate over directory entries by walking
-// each directory in the tree, including baseDir.
+// each file or directory in the tree, including baseDir.
 //
 // The n parameter follows the semantics of fs.ReadDirFile:
 // https://pkg.go.dev/io/fs@latest#ReadDirFile.
@@ -45,11 +45,11 @@ func NewReadDirIterRecursive(baseDir string, openDir OpenReadDirCloserFunc, n in
 	}
 }
 
-func walkDir(baseDir string, openDir OpenReadDirCloserFunc, n int, skipDir *bool, yield func(*DirAndEntry, error) bool) {
+func walkDir(baseDir string, openDir OpenReadDirCloserFunc, n int, skipDir *bool, yield func(*DirAndEntry, error) bool) bool {
 	dirFile, err := openDir(baseDir)
 	if err != nil {
 		yield(nil, err)
-		return
+		return true
 	}
 	defer func() {
 		if err := dirFile.Close(); err != nil {
@@ -61,13 +61,13 @@ func walkDir(baseDir string, openDir OpenReadDirCloserFunc, n int, skipDir *bool
 	for entry, err := range NewReadDirIter(dirFile, n) {
 		if err != nil {
 			yield(nil, err)
-			return
+			return true
 		}
 		if !yield(&DirAndEntry{
 			dir:   baseDir,
 			entry: entry,
 		}, nil) {
-			return
+			return true
 		}
 		if entry.IsDir() {
 			if skipDir != nil && *skipDir {
@@ -76,7 +76,59 @@ func walkDir(baseDir string, openDir OpenReadDirCloserFunc, n int, skipDir *bool
 			}
 
 			subDir := filepath.Join(baseDir, entry.Name())
-			walkDir(subDir, openDir, n, skipDir, yield)
+			if walkDir(subDir, openDir, n, skipDir, yield) {
+				return true
+			}
 		}
 	}
+	return false
+}
+
+// NewReadDirIterRecursiveSorted returns an iterate over directory entries by walking
+// each file or directory in the tree, including baseDir.
+//
+// Note: The directory entries are in lexical order in each directory.
+func NewReadDirIterRecursiveSorted(baseDir string, openDir OpenReadDirCloserFunc, skipDir *bool) iter.Seq2[*DirAndEntry, error] {
+	return func(yield func(*DirAndEntry, error) bool) {
+		walkDirSorted(baseDir, openDir, skipDir, yield)
+	}
+}
+
+func walkDirSorted(baseDir string, openDir OpenReadDirCloserFunc, skipDir *bool, yield func(*DirAndEntry, error) bool) bool {
+	dirFile, err := openDir(baseDir)
+	if err != nil {
+		yield(nil, err)
+		return true
+	}
+	defer func() {
+		if err := dirFile.Close(); err != nil {
+			yield(nil, err)
+			return
+		}
+	}()
+
+	for entry, err := range NewReadDirIterSorted(dirFile) {
+		if err != nil {
+			yield(nil, err)
+			return true
+		}
+		if !yield(&DirAndEntry{
+			dir:   baseDir,
+			entry: entry,
+		}, nil) {
+			return true
+		}
+		if entry.IsDir() {
+			if skipDir != nil && *skipDir {
+				*skipDir = false
+				continue
+			}
+
+			subDir := filepath.Join(baseDir, entry.Name())
+			if walkDirSorted(subDir, openDir, skipDir, yield) {
+				return true
+			}
+		}
+	}
+	return false
 }
